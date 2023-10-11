20da6edb7e89cb9d37f40eb3d11dfd10b94a2c6f
Frank <kamunyuz@gmail.com>
2023-10-11 19:25:08 +0300

    basemodel

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import models
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now
+
+    def to_dict(self):
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
