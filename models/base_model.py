39bdf5686b15f0643c17e5d3c87dc770b949dfe6
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-10-12 14:36:13 +0300

    BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,19 @@ from datetime import datetime
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == 'created_at' and type(self.created_at) is str:
+                    value = datetime.strptime(value, time)
+                if key == 'updated_at' and type(self.updated_at) is str:
+                    value = datetime.strptime(value, time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
