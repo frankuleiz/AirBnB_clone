543e9b9e23ae4d91106adc86539e27bf58b5bb6d
Frank <kamunyuz@gmail.com>
2023-10-13 19:33:02 +0300

    console documentation

--- console.py
+++ console.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This is a module for the console module
+Defines the class HBNBCommand
+"""
 import cmd
 
 
@@ -7,18 +11,23 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
+        """Quit the program"""
         return True
 
     def do_EOF(self, arg):
+        """Terminates the console incase of the input is CTRL + D"""
         return True
 
     def emptyline(self):
+        """Does not execute anything"""
         pass
 
     def help_quit(self):
+        """Documentation for the help command"""
         print("Quit command to exit the program\n")
 
     def help_EOF(self):
+        """Documentation for the EOF command"""
         print("Exit the program using EOF (Ctrl+D)")
 
 
